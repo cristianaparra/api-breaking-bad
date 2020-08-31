512314e256864d1e2387fab4971f2fe24f294663
Cristian Parra <cristian.a.parra@gmail.com>
2020-08-31 17:02:11 -0400

    api con async await

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from '@emotion/styled'
 
+const Boton = styled.button`
+background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+background-size: 300px;
+font-family: Arial, Helvetica, sans-serif;
+color: #fff;
+margin-top: 3rem;
+padding: 1rem 3rem;
+font-size: 2rem;
+border: 2px solid black;
+`;
+const Contenedor = styled.div`
+display: flex;
+align-items: center;
+padding-top: 5rem;
+flex-direction: column;
+`
 
 function App() {
+  const consultandoApi = async () => {
+   
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json();
+    console.log(frase[0])
+  }
+
   return (
-   <h1>Breakingbad</h1> 
+    <Contenedor>
+      <Boton
+        onClick={consultandoApi}
+      >Obtener frase</Boton>
+    </Contenedor>
+
+
   );
 }
 
